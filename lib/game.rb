a466d7d2b6b24911f77fa3d9718db424e3cb974d
deniedwin <24278798+deniedwin@users.noreply.github.com>
2025-06-10 15:18:39 +0200

    Made lib file to input new file to make it OOP

---
+++ lib/game.rb
@@ -0,0 +1,5 @@
+class Game
+  def start_game
+    puts "game has started"
+  end
+end
--- main.rb
+++ main.rb
@@ -1,7 +1,22 @@
 # main file of the project hangman
 # 
-words = File.open("google-10000-english-no-swears.txt") do |file|
-  file.read.split
+# words = File.open("google-10000-english-no-swears.txt") do |file|
+#   file.read.split
+# end
+
+# puts words.inspect
+
+# requiered files inside lib folder for app to run correctly
+require_relative 'lib/game'
+require_relative 'lib/gameio'
+require_relative 'lib/state'
+
+# class that runs the game
+class GameRunner
+  def self.run
+    hangman = Game.new
+    hangman.start_game
+  end
 end
 
-puts words.inspect
+GameRunner.run
