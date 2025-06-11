744063e7d84c1c1ea27b10729f711fa5c09d9c9f
deniedwin <24278798+deniedwin@users.noreply.github.com>
2025-06-11 14:57:51 +0200

    Made funciton that selects specific lenght words

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,14 @@
 class Game
   def start_game
-    puts "game has started"
+    puts "game started"
+    select_secret_word
+  end
+
+  def select_secret_word
+    words = File.open('google-10000-english-no-swears.txt', 'r') do |file|
+      file.read.split
+    end
+    
+    puts words.select { |word| word.length >= 5 && word.length <= 12 }
   end
 end
