22eb9966e50781ac47717368ae9030f0edabb84a
deniedwin <24278798+deniedwin@users.noreply.github.com>
2025-06-11 15:03:06 +0200

    Made function to select random word

--- lib/game.rb
+++ lib/game.rb
@@ -8,7 +8,8 @@ class Game
     words = File.open('google-10000-english-no-swears.txt', 'r') do |file|
       file.read.split
     end
-    
-    puts words.select { |word| word.length >= 5 && word.length <= 12 }
+    filter_words = words.select { |word| word.length >= 5 && word.length <= 12 }
+    secret_word = filter_words.sample
+    secret_word
   end
 end
